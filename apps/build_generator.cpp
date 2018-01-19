3d6e7287968a721c903936edebe1f58efe6c8f33
antoniomuso <antoniomusolino007@gmail.com>
2018-01-19 10:14:06 +0100

    created project file

---
+++ apps/build_generator.cpp
@@ -0,0 +1,15 @@
+//
+// Created by antoniomusolino on 19/01/18.
+//
+
+#define YGL_OPENGL 1
+#include "../yocto/yocto_gl.h"
+using namespace ygl;
+
+int main () {
+    auto vettore = vec3f {0,0,0};
+    
+    print("{} {}", vettore, vettore);
+
+
+}
---
+++ cmake-build-debug/CMakeFiles/build_generator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/build_generator.dir/apps/build_generator.cpp.o"
+  "../bin/build_generator.pdb"
+  "../bin/build_generator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/build_generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
