bff2f6f0bec79610c5ffd509c288b0fda3ede301
antoniomuso <antoniomusolino007@gmail.com>
2018-01-19 13:21:26 +0100

    test obj insert

--- apps/build_generator.cpp
+++ apps/build_generator.cpp
@@ -6,10 +6,77 @@
 #include "../yocto/yocto_gl.h"
 using namespace ygl;
 
-int main () {
-    auto vettore = vec3f {0,0,0};
+void add_instance(scene* scn, const std::string& name, const frame3f& f,
+                  shape* shp) {
 
-    print("{} {}", vettore, vettore);
 
+    scn->materials.push_back(shp->mat);
+    scn->shapes.push_back(shp);
+    auto ist = new instance;
+    ist->name = name;
+    ist->shp = shp;
+    ist->frame = f;
+    scn->instances.push_back(ist);
+}
+
+
+void load_and_add_to_scen (const string& filename, scene *scene) {
+    auto obj = load_scene(filename);
+    print("{}", obj->shapes.at(0)->);
+
+    add_instance(scene,
+                 "Object:"+ to_string(scene->instances.size()),
+                 make_frame_fromz({-1.25f, 1, 0}, {0, 0, 1}),
+                 obj->shapes.at(0));
 
 }
+
+
+int main () {
+    auto scen = new scene();
+
+    // add light
+    auto lshp = new shape{"light"};
+    lshp->pos = {{1.4f, 8, 6}, {-1.4f, 8, 6}};
+    lshp->points = {0, 1};
+    auto lmat = new material{"light"};
+    lmat->ke = {100, 100, 100};
+    lshp->mat = lmat;
+
+    scen->shapes.push_back(lshp);
+    scen->materials.push_back(lmat);
+    scen->instances.push_back(
+            new instance{"light", identity_frame3f, lshp});
+
+    // add cam
+    auto cam = new camera{"cam"};
+    cam->frame = lookat_frame3f({0, 4, 10}, {0, 1, 0}, {0, 1, 0});
+    cam->yfov = 15 * pif / 180.f;
+    cam->aspect = 16.0f / 9.0f;
+    cam->aperture = 0;
+    cam->focus = length(vec3f{0, 4, 10} - vec3f{0, 1, 0});
+    scen->cameras.push_back(cam);
+
+    // add floor
+
+    auto mat = new material{"floor"};
+    mat->kd = {0.2f, 0.2f, 0.2f};
+    scen->materials.push_back(mat);
+    auto shp = new shape{"floor"};
+    shp->mat = mat;
+    shp->pos = {{-20, 0, -20}, {20, 0, -20}, {20, 0, 20}, {-20, 0, 20}};
+    shp->norm = {{0, 1, 0}, {0, 1, 0}, {0, 1, 0}, {0, 1, 0}};
+    shp->texcoord = {{-10, -10}, {10, -10}, {10, 10}, {-10, 10}};
+    shp->triangles = {{0, 1, 2}, {0, 2, 3}};
+    scen->shapes.push_back(shp);
+    scen->instances.push_back(new instance{"floor", identity_frame3f, shp});
+
+
+
+
+    load_and_add_to_scen("Models/modularBuildings_001.obj",scen);
+    save_scene("./file.obj",scen,save_options());
+}
+
+
+
